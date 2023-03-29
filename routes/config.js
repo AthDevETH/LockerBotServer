c3a3ffc643614dc18cf41a14c67656daeacf7b53
joesawtw1 <joesawtw@gmail.com>
2023-03-29 18:07:15 +0800

    updated route path

--- routes/config.js
+++ routes/config.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const validation = require("../middleware/validation");
 
 const config = require("../config");
-router.get("/config", validation, async (req, res) => {
+router.get("/", validation, async (req, res) => {
   return res.json(config);
 });
 
